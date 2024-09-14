4100551b4c4dcb2981854282a1bb43ce5d2b99fc
ayomisco <fayomide324@gmail.com>
2024-09-14 20:59:46 +0100

    updated the Ajax, admin, correct all errors

--- votify-Backend/staticfiles/js/candidate_filter.js
+++ votify-Backend/staticfiles/js/candidate_filter.js
@@ -1,13 +1,18 @@
-// static/js/candidate_filter.js
+// js/candidate_filter.js
+
 $(document).ready(function () {
     function fetchCandidates() {
         $('#loadingSpinner').show();  // Show spinner while loading
+        var $jq = jQuery.noConflict();
+
         $.ajax({
             url: '/candidates/',
             data: {
                 'election_type': $('#electionFilter').val()
             },
             success: function (data) {
+                console.log(data);  // Log the response for debugging
+
                 var candidateTable = $('#candidateTable tbody');
                 candidateTable.empty();  // Clear the table before adding new rows
 
@@ -22,15 +27,17 @@ $(document).ready(function () {
                             positionClass = 'runner-up';
                         } else if (candidate.position === 'Second Runner-up') {
                             positionClass = 'second-runner-up';
+                        } else if (candidate.position === 'No Votes Yet') {
+                            positionClass = 'no-votes';  // Class for candidates with no votes
                         }
-
+                        console.log(`Position Class: ${positionClass}`); // Add this line to debug
                         candidateTable.append(`
-                            <tr class="${positionClass}">
+                            <tr class="">
                                 <td>${index + 1}</td>
                                 <td>${candidate.full_name}</td>
                                 <td>${candidate.department}</td>
                                 <td>${candidate.votes_count}</td>
-                                <td><span class="btn btn-success">${candidate.position}</span></td>
+                                <td><span class="btn ${positionClass} ">${candidate.position}</span></td>
                             </tr>
                         `);
                     });
